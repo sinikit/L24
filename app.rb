de0cbed7e7fd1584a309738e8fe6fd26117b2a30
Sinikit <sinikit@yahoo.com>
2017-06-13 23:01:50 +0600

    BarberShop

---
+++ app.rb
@@ -0,0 +1,18 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+get '/visits' do
+	erb :visits
+end
+get '/contacts' do
+	erb :contacts
+end
