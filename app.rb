1668f8a4aae0fb73a8b6d5e4d05ae630565f949a
sinikit <sinikit@yahoo.com>
2017-06-14 14:29:17 +0600

    Add Visitirs to file

--- app.rb
+++ app.rb
@@ -13,6 +13,22 @@ end
 get '/visits' do
 	erb :visits
 end
+
+post '/visits' do
+	@customername = params[:username]
+	@phonenumber = params[:phonenumber]
+	@datatime = params[:datatime]
+	tofile "#{@customername} will come at #{@datatime}. You can contact him by #{@phonenumber}. \n" , "visitors_list"
+	@welcomecustomer = "Dear #{@customername}, will happy to see you at #{@datatime}"
+	erb :visits 
+end
+
 get '/contacts' do
 	erb :contacts
+end
+
+def tofile userdata ,  file_name
+	f=File.open("./public/#{file_name}.txt", "a")
+	f.print userdata
+	f.close
 end
