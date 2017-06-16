76bd2642884154351cd81f21ec81da6f50dc550a
sinikit <sinikit@yahoo.com>
2017-06-16 11:30:29 +0600

    Add colorPicker

--- app.rb
+++ app.rb
@@ -19,8 +19,9 @@ post '/visits' do
 	@phonenumber = params[:phonenumber]
 	@datatime = params[:datatime]
 	@specialist = params[:spec]
-	tofile "#{@customername} will come at #{@datatime}. #{@specialist} can contact him by #{@phonenumber}. \n" , "visitors_list"
-	@welcomecustomer = "Dear #{@customername}, #{@specialist} will happy to see you at #{@datatime}"
+	@color = params[:color]
+	tofile "#{@color}  #{@customername} will come at #{@datatime}. #{@specialist} can contact him by #{@phonenumber}. \n" , "visitors_list"
+	@welcomecustomer = "Dear #{@color}  #{@customername}, #{@specialist} will happy to see you at #{@datatime}"
 	erb :visits 
 end
 
